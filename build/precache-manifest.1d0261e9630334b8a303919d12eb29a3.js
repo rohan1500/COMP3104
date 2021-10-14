d50a6da0f8082268906b15bd4b6092c79cf6e037
Rohan Khullar <101284533@georgebrown.ca>
2021-10-13 20:18:10 -0400

    .travis.yml file is updated again

---
+++ build/precache-manifest.1d0261e9630334b8a303919d12eb29a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63018f72bc13b56029b0",
+    "url": "/rohan1500/COMP3104/static/css/main.37cf9cde.chunk.css"
+  },
+  {
+    "revision": "63018f72bc13b56029b0",
+    "url": "/rohan1500/COMP3104/static/js/main.63018f72.chunk.js"
+  },
+  {
+    "revision": "d26b7b7f845c6ece14d8",
+    "url": "/rohan1500/COMP3104/static/js/runtime~main.d26b7b7f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/rohan1500/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "1a146b66778e517661d7ae78a5f99c99",
+    "url": "/rohan1500/COMP3104/index.html"
+  }
+];
